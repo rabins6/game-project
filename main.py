5c12fe7e7eec59f26f9dbff43672927d17f8d38c
Rabin <rabin.shrestha@metropolia.fi>
2023-10-10 16:07:58 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+connection = mysql.connector.connect(
+          host='127.0.0.1',
+          port=3306,
+          database='flight_game',
+          user='',
+          password='',
+          autocommit=True
+          )
+
+
